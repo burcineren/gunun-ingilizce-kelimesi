6324b1ef65a01f13d2edd3c5256533d3f0606786
burcineren <burcin.ern969@gmail.com>
2022-10-29 18:32:37 +0300

    add dotenv to project structure

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express')
+import * as dotenv from 'dotenv' 
+dotenv.config()
+import express from 'express'
 const app = express()
+
+// console.log(process.env.PORT);
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
